66aa6e4cfef31271d3df45b8ecf3962f28b020dc
Javier Villanueva <javiervd@gmail.com>
2019-08-15 11:39:37 +0100

    Prefetch data server side automatically

--- components/cmsBlock.js
+++ components/cmsBlock.js
@@ -1,4 +1,4 @@
-import { parse } from 'node-html-parser';
+import { parse } from 'node-html-parser'
 
 export default {
   props: {
@@ -13,37 +13,47 @@ export default {
       required: false
     }
   },
+  serverPrefetch() {
+    return this.fetchBlock();
+  },
   created() {
-    let queryKey = '';
-    let queryValue = '';
-
-    if (this.id) {
-      queryKey = 'id';
-      queryValue = this.id;
-    } else if (this.identifier) {
-      queryKey = 'identifier';
-      queryValue = this.identifier;
+    if (!this.data) {
+      return this.fetchBlock();
     }
+  },
+  methods: {
+    fetchBlock() {
+      let queryKey = ''
+      let queryValue = ''
 
-    if (queryKey && queryValue) {
-      this.$store.dispatch('cmsBlock/single', {
-        key: queryKey,
-        value: queryValue
-      });
+      if (this.id) {
+        queryKey = 'id'
+        queryValue = this.id
+      } else if (this.identifier) {
+        queryKey = 'identifier'
+        queryValue = this.identifier
+      }
+
+      if (queryKey && queryValue) {
+        return this.$store.dispatch('cmsBlock/single', {
+          key: queryKey,
+          value: queryValue
+        })
+      }
     }
   },
   computed: {
     data() {
       if (this.id) {
-        return this.$store.getters[`cmsBlock/cmsBlockId`](this.id);
+        return this.$store.getters[`cmsBlock/cmsBlockId`](this.id)
       } else if (this.identifier) {
         return this.$store.getters[`cmsBlock/cmsBlockIdentifier`](
           this.identifier
-        );
+        )
       }
     },
     parsedContent() {
-      return parse(this.data.content);
+      return parse(this.data.content)
     }
   }
-};
+}
